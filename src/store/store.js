f6bf380cb807310a164662038188336f4e72f346
Caio <caio.bozato@ibm.com>
2019-07-08 17:54:56 -0300

    Finished products addition functionality

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ export default new Vuex.Store({
         produtos: [
             { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
             { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-            { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 }
+            { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
         ]
     }
 });
