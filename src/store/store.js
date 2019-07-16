aa815880c77b35c115edd4cc1ba802d8d318d6c1
Caio <caio.bozato@ibm.com>
2019-07-16 11:13:04 -0300

    Added external getters

--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,21 @@ Vue.use(Vuex);
 import carrinho from './modules/carrinho';
 import parametros from './modules/parametros';
 
+import * as getters from './getters';
+//alternative:
+//import {getNome, getSobrenome} from './getters';
+
 export default new Vuex.Store({
+    state : {
+        nome: 'Caio',
+        sobrenome: 'Bozato Aroca'
+    },
+    getters,
+    //alternative:
+    /*
+    getters: {
+        ...getters
+    }
+    */
     modules: { carrinho, parametros }
 });
